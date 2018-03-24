874e9d93c5f4c93b479654b152bd1fd3ed127f46
Vasya Kortik <37380184+GreatLolk@users.noreply.github.com>
2018-03-24 16:20:31 +0300

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeaBattle
+{
+    class Point
+    {
+        public int typeofpoint = 0;
+
+        public void RenderPoint(Point point)
+        {
+            if (typeofpoint == 0)
+                Console.Write("."); // Empty
+
+            else if (typeofpoint == 1)
+                Console.Write("*"); // Ship
+
+            else if (typeofpoint == 2)
+                Console.Write("X"); // Damaged
+
+            else if (typeofpoint == 3)
+                Console.Write("O"); // Miss
+        }
+    }
+}
---
+++ Renderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeaBattle
+{
+    class Renderer
+    {
+        public void Render()
+        {
+            Console.WriteLine("        A    Б    В    Г    Д   Е    Ж    З   И    К "); // 1 string
+            Console.WriteLine("---+---+---+---+---+---+---+---+---+---+---+"); // 2 stirng
+            Console.WriteLine(" 1  |       |       |      |       |  *  |       |       |      |      |      |"); // 3 string
+            Console.WriteLine("---+---+---+---+---+---+---+---+---+---+---+"); // 4 string
+            Console.WriteLine("2  |       |       |      |       |  *  |       |       |      |      |      |"); // 5 string
+            Console.WriteLine("---+---+---+---+---+---+---+---+---+---+---+"); // 6 string
+            Console.WriteLine(" 3  |       |       |      |       |      |       |       |      |      |      |"); // 7 string
+            Console.WriteLine("---+---+---+---+---+---+---+---+---+---+---+"); // 8 string
+            Console.WriteLine(" 4  |       |       |      |       |      |       |       |      |      |      |"); // 9 string
+            Console.WriteLine("---+---+---+---+---+---+---+---+---+---+---+"); // 10 stirng
+            Console.WriteLine(" 5  |       |       |      |  *  |  * |       |       |      |      |      |"); // 11 stirng
+            Console.WriteLine("---+---+---+---+---+---+---+---+---+---+---+"); // 12 string
+            Console.WriteLine(" 6  |       |       |      |       |      |       |       |      |      |      |"); // 13 string
+            Console.WriteLine("---+---+---+---+---+---+---+---+---+---+---+"); // 14 string
+            Console.WriteLine(" 7  |       |       |      |       |      |       |       |      |      |      |"); // 15 string
+            Console.WriteLine("---+---+---+---+---+---+---+---+---+---+---+"); // 16 string
+            Console.WriteLine(" 8  |       |       |      |       |      |       |       |      |      |      |"); // 17 string
+            Console.WriteLine("---+---+---+---+---+---+---+---+---+---+---+"); // 18 string
+            Console.WriteLine("9 |       |       |      |       |      |       |       |      |      |      |"); // 19 string
+            Console.WriteLine("---+---+---+---+---+---+---+---+---+---+---+"); // 20 string
+            Console.WriteLine("10 |       |       |      |       |      |       |       |      |      |      |"); // 21 string
+            Console.WriteLine("---+---+---+---+---+---+---+---+---+---+---+"); // 22 string
+
+        }
+    }
+}
